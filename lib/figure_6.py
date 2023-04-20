98ae377eca1443bbae1baff67a0033ffd67f852d
PHL <s1948149@ed.ac.uk>
2023-04-20 12:05:39 +0100

    updated function

---
+++ lib/figure_6.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# using firing during intertrial interval (ITI) window -1 to -0.5ms
+def figure_5_panel_c(pfc_times: np.ndarray, str_times: np.ndarray, cue_times: np.ndarray, pfc_name: str, str_name: str, monosynaptic=False):
+    # calculate the cross correlation of the two signals during a centered 20-trial long window
+    pass
+
+# using firing during 1-3ms after cue
+def figure_5_panel_e(pfc_bg: np.ndarray, str_bg: np.ndarray, pfc_name: str, str_name: str, monosynaptic=False):
+    # calculate the cross correlation of the two signals during a centered 20-trial long window
+    pass
